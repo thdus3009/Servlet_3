ed7158190403d043b7dece4b7be6e8fef703cadc
thdus3009 <63095234+thdus3009@users.noreply.github.com>
2020-04-08 11:27:05 +0900

    AddDBConnect

--- WebContent/WEB-INF/views/index.jsp
+++ WebContent/WEB-INF/views/index.jsp
@@ -18,7 +18,7 @@
       <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="${pageContext.request.contextPath}">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Page 1-1</a></li>
@@ -26,7 +26,7 @@
           <li><a href="#">Page 1-3</a></li>
         </ul>
       </li>
-      <li><a href="#">Page 2</a></li>
+      <li><a href="${pageContext.request.contextPath}/point/pointList">Point</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
@@ -51,5 +51,6 @@
 </div>
 
 	<h1>Add Point Branch</h1>
+
 </body>
 </html>
--- src/com/sy/point/PointController.java
+++ src/com/sy/point/PointController.java
@@ -1,6 +1,8 @@
 package com.sy.point;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -42,21 +44,49 @@ public class PointController extends HttpServlet {
 		//path를 담을 변수 (어디로보낼건지 > path)
 		String path = "";
 		
+		//forward인지 redirect인지 
 		if(command.equals("/pointList")) {
-			System.out.println("list");
+			
+			
+			check = true;//check는 false일때만 써주면 된다.
+			path = "../WEB-INF/views/point/pointList.jsp";
+			
 		}else if(command.equals("/pointAdd")){
-			System.out.println("add");
+			if(method.equals("POST")) {
+				
+			}else {
+				path="../WEB-INF/views/point/pointAdd.jsp";
+			}
+			
+			
 		}else if(command.equals("/pointMod")) {
-			System.out.println("mod");
+			if(method.equals("POST")) {
+				
+			}else {
+				path="../WEB-INF/views/point/pointMod.jsp";
+			}
+			
 		}else if(command.equals("/pointSelect")) {
-			System.out.println("select");
+			
+			path = "../WEB-INF/views/point/pointSelect.jsp";
+			
 		}else if(command.equals("/pointDelete")) {
-			System.out.println("delete");
+
+			path = "../WEB-INF/views/point/pointDelete.jsp";
 		}else {
 			System.out.println("etc");
 		}
 		
+		//url주소는 path에 담고있다.//check가 true면 Forward,false면 redirect 선택
+		if(check) {//forward
+			RequestDispatcher view = request.getRequestDispatcher(path);
+			view.forward(request, response);
+		}else {//redirect
+			response.sendRedirect(path);
+		}
+		
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
